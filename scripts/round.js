3124baf6e06db7e738fc5cbef133975ca8c97a2b
Michael Fenwick <mickyfen17@aol.com>
2017-02-06 20:41:58 -0700

    build up round constructor and add further passing tests

--- scripts/round.js
+++ scripts/round.js
@@ -1,6 +1,22 @@
+import Guess from "./guess";
+
 class Round {
-  constructor(cardDeck) {
+  constructor({ cardDeck }) {
     this.deck = cardDeck;
+    this.guesses = [];
+    this.count = 0;
+  }
+  currentCard() {
+    // console.log(this.deck.questionDeck)
+    return this.deck.questionDeck.shift();
+    // console.log(this.deck.questionDeck.shift());
+    // console.log(this.deck.questionDeck)
+  }
+  recordGuess(inputGuess) {
+    let guess = new Guess({ response: inputGuess, questionCard: this.currentCard() });
+    this.guesses.push(guess);
+    this.count++;
+    return this.guesses[0];
   }
 }
 
--- tests/round-test.js
+++ tests/round-test.js
@@ -9,4 +9,96 @@ describe("testing round constructor", () => {
     assert.isFunction(Round);
   });
 
+  it("round.deck should hold an object", () => {
+    let card1 = new Card({ question: "What is the capital of Alaska?", answer: "Juneau" });
+    let deck = new Deck();
+    deck.addCardToDeck(card1);
+    let round = new Round({ cardDeck: deck });
+    expect(round).to.have.property("deck").that.is.an("object");
+  });
+
+  it("round.deck.questionDeck should be an array", () => {
+    let card1 = new Card({ question: "What is the capital of Alaska?", answer: "Juneau" });
+    let deck = new Deck();
+    deck.addCardToDeck(card1);
+    let round = new Round({ cardDeck: deck });
+    expect(round).to.have.property("deck").that.is.an("object").with.deep.property("questionDeck").that.is.an("array");
+  });
+
+  it("round should accept a deck of cards and prove that they exist within round", () => {
+    let card1 = new Card({ question: "What is the capital of Alaska?", answer: "Juneau" });
+    let card2 = new Card({ question: "Approximately how many miles are in one astronomical unit?", answer: "93,000,000" });
+    let deck = new Deck();
+    deck.addCardToDeck(card1);
+    deck.addCardToDeck(card2);
+    let round = new Round({ cardDeck: deck });
+    expect(round.deck.questionDeck).to.include(card1);
+    expect(round.deck.questionDeck).to.include(card2);
+  });
+
+  it("round guesses should return an array", () => {
+    let round = new Round({});
+    assert.isArray(round.guesses);
+  });
+
+  it("round should accept a deck of cards and display the first of those cards using current card", () => {
+    let card1 = new Card({ question: "What is the capital of Alaska?", answer: "Juneau" });
+    let card2 = new Card({ question: "Approximately how many miles are in one astronomical unit?", answer: "93,000,000" });
+    let deck = new Deck();
+    deck.addCardToDeck(card1);
+    deck.addCardToDeck(card2);
+    let round = new Round({ cardDeck: deck });
+    assert.equal(round.currentCard(), card1);
+  });
+
+  it("round recordGuess should accept a userGuess that is passed into guesses array as a response on Guess constructor", () => {
+    let card1 = new Card({ question: "What is the capital of Alaska?", answer: "Juneau" });
+    let deck = new Deck();
+    deck.addCardToDeck(card1);
+    let round = new Round({ cardDeck: deck });
+    let userGuess = "Juneau";
+    round.recordGuess(userGuess);
+    assert.equal(round.guesses[0].response, userGuess);
+  });
+
+  it("round guesses array should increase in length from 0 to 1 as userGuess is passed into recordGuess", () => {
+    let card1 = new Card({ question: "What is the capital of Alaska?", answer: "Juneau" });
+    let deck = new Deck();
+    deck.addCardToDeck(card1);
+    let round = new Round({ cardDeck: deck });
+    expect(round.guesses).to.have.lengthOf(0);
+    round.recordGuess("Juneau");
+    expect(round.guesses).to.have.lengthOf(1);
+  });
+
+  it("round count should increase from 0 to 1 as userGuess is passed into recordGuess", () => {
+    let card1 = new Card({ question: "What is the capital of Alaska?", answer: "Juneau" });
+    let deck = new Deck();
+    deck.addCardToDeck(card1);
+    let round = new Round({ cardDeck: deck });
+    assert.equal(round.count, 0);
+    round.recordGuess("Juneau");
+    assert.equal(round.count, 1);
+  });
+
+  it("should be able to get feedback of Correct! following correct userGuess", () => {
+    let card1 = new Card({ question: "What is the capital of Alaska?", answer: "Juneau" });
+    let deck = new Deck();
+    deck.addCardToDeck(card1);
+    let round = new Round({ cardDeck: deck });
+    let userGuess = "Juneau";
+    round.recordGuess(userGuess);
+    assert.equal(round.guesses[0].feedback(), "Correct!");
+  });
+
+  it("should be able to get feedback of Incorrect following wrong userGuess", () => {
+    let card1 = new Card({ question: "Which planet is closest to the sun?", answer: "Mercury" });
+    let deck = new Deck();
+    deck.addCardToDeck(card1);
+    let round = new Round({ cardDeck: deck });
+    let userGuess = "Saturn";
+    round.recordGuess(userGuess);
+    assert.equal(round.guesses[0].feedback(), "Incorrect");
+  });
+
 });
