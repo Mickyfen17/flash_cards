1574df2cf02c21404630fdf2e7fe5bb1b8fd1677
Michael Fenwick <mickyfen17@aol.com>
2017-02-08 11:39:59 -0700

    move txtSpliter to be a prototype function on CardGenerator & alter tests

--- scripts/card-generator.js
+++ scripts/card-generator.js
@@ -1,6 +1,16 @@
+import Card from "../scripts/card";
+
 class CardGenerator {
   constructor({ cards }) {
-    this.cards = cards;
+    this.cards = this.txtSpliter(cards);
+  }
+  txtSpliter(txtArray) {
+    return txtArray.map( card => {
+      let cardSplit = card.split(",");
+      let txtQuestion = cardSplit[0];
+      let txtAnswer = cardSplit[1];
+      return new Card({ question: txtQuestion, answer: txtAnswer });
+    });
   }
 }
 
--- tests/card-generator-test.js
+++ tests/card-generator-test.js
@@ -5,15 +5,6 @@ import fs from 'fs';
 
 const words = fs.readFileSync('./cards.txt', "utf8").split("\n");
 
-const txtSpliter = txtArray => {
-  return words.map( card => {
-    let cardSplit = card.split(",");
-    let txtQuestion = cardSplit[0];
-    let txtAnswer = cardSplit[1];
-    return new Card({ question: txtQuestion, answer: txtAnswer });
-  });
-};
-
 describe("testing card generator constructor", () => {
 
   it("card generator should be a function", () => {
@@ -21,25 +12,30 @@ describe("testing card generator constructor", () => {
   });
 
   it("cardGenerator cards should hold an array", () => {
-    let cardGeneratorArr = txtSpliter(words);
-    let cardGenerator = new CardGenerator({ cards: cardGeneratorArr });
+    let cardGenerator = new CardGenerator({ cards: words });
 
+    cardGenerator.txtSpliter(words);
     assert.isArray(cardGenerator.cards);
   });
 
+  it("cardGenerator txtSpliter should return an array", () => {
+    let cardGenerator = new CardGenerator({ cards: words });
+
+    expect(cardGenerator.txtSpliter(words)).to.be.instanceof(Array);
+  });
+
   it("cardGenerator should split the txt file, add question and answer into Card, pass Cards into cardGenerator.cards and check if they exist", () => {
-    let cardGeneratorArr = txtSpliter(words);
-    let cardGenerator = new CardGenerator({ cards: cardGeneratorArr });
+    let cardGenerator = new CardGenerator({ cards: words });
 
     expect(cardGenerator).to.have.property("cards").that.is.an("array").with.deep.property("0").that.is.an("object");
   });
 
   it("cardGenerator should split the txt file, add question and answer into Card, pass Cards into cardGenerator.cards and check for keys question and answer", () => {
-    let cardGeneratorArr = txtSpliter(words);
-    let cardGenerator = new CardGenerator({ cards: cardGeneratorArr });
+    let cardGenerator = new CardGenerator({ cards: words });
 
     expect(cardGenerator).to.have.property("cards").that.is.an("array").with.deep.property("1").that.is.an("object").with.deep.property("question");
     expect(cardGenerator).to.have.property("cards").that.is.an("array").with.deep.property("1").that.is.an("object").with.deep.property("answer");
   });
+
 
 });
