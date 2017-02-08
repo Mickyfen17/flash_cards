fb4dfc430f3cc66c0df56ecf459178bfb7f0015f
Michael Fenwick <mickyfen17@aol.com>
2017-02-07 21:10:14 -0700

    add first passing test

--- tests/card-generator-test.js
+++ tests/card-generator-test.js
@@ -1,2 +1,10 @@
 import { expect, assert } from 'chai';
 import CardGenerator from "../scripts/card-generator";
+
+describe("testing card generator constructor", () => {
+
+  it("card generator should be a function", () => {
+    assert.isFunction(CardGenerator);
+  });
+
+});
